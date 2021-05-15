88b9a605afe1b0d3835128cd31f1a6c594c669e9
sumanth8ch <43502277+sumanth8ch@users.noreply.github.com>
2021-05-15 21:07:44 +0530

    Create stack.cpp

---
+++ C++/Datastructures/stack.cpp
@@ -0,0 +1,68 @@
+/*The stack operations are given below:
+Stack(): creates a new stack that is empty.
+push(item) : adds a new item to the top of the stack. 
+pop() : removes the top item from the stack.
+peek() : returns the top item from the stack but does not remove it. 
+isEmpty() : tests to see whether the stack is empty.*/
+
+#include<iostream>
+using namespace std;
+#define stacksize 1000
+
+class Stack{
+    int topindex;
+public:
+    int a[stacksize];
+    Stack(){topindex = -1;}
+    bool push(int value);
+    int pop();
+    int peek();
+    int length();
+    bool isEmpty();
+};
+
+bool Stack::isEmpty(){
+    return topindex <0;
+}
+
+bool Stack::push(int value){
+    if (topindex>= stacksize-1){
+        cout<< "Stack size limit reached";
+        return false;
+    }
+    else {
+        a[++topindex] = value;
+        return true;
+    }
+}
+
+int Stack::pop(){
+    if (topindex < 0) {
+            cout<<"Stack is empty";
+            return 0;}
+    else{
+        int x = a[topindex--];
+        return x;
+    }
+}
+
+int Stack::peek(){
+    if (topindex<0){
+        return 0;
+    }
+    else{int val = a[topindex];
+        return val;}
+}
+
+
+
+int main(){
+    class Stack s;
+    s.push(3);
+    s.push(6);
+    s.push(2);
+    s.push(89);
+    cout<< s.pop() << "popped out\n";
+    cout<<s.peek();
+    return 0;
+}
